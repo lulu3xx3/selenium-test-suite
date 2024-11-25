4c7cfb34d11f7481a611a10e732202f01e58f3d4
varun <jm.varun222@gmail.com>
2024-11-25 14:52:13 +0530

    added important info as a comment for test case 2 and added a data provider for it, added test case 3 class and data provider which is fully working

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,84 +8,124 @@ import utils.BaseTest;
 
 public class MainTest extends BaseTest {
 
-    @Test(dataProvider = "DataForTestCase1",groups = "testCase1")
+    @Test(dataProvider = "DataForTestCase1", groups = "testCase1")
     public void RegisterUser(String gender, String password, String dobDay, String dobMonth, String dobYear,
                              String firstName, String lastName, String companyName, String companyAddress,
-                             String countryName, String state, String city, String zipCode, String mobileNumber,String email){
+                             String countryName, String state, String city, String zipCode, String mobileNumber, String email) {
         LandingPage landingPage = new LandingPage(driver);
         landingPage.openURL("https://automationexercise.com/");
-        Assert.assertEquals(driver.getTitle(),"Automation Exercise","The title is not as expected!");
+        Assert.assertEquals(driver.getTitle(), "Automation Exercise", "The title is not as expected!");
         AbstractReusable abstractReusable = new AbstractReusable(driver);
         abstractReusable.clickOnSignupLoginButton();
         String newUserH2 = driver.findElement(By.xpath("//h2[text()='New User Signup!']")).getText();
-        Assert.assertTrue(newUserH2.contains("New User Signup!"),"The h2 message is not as expected!");
+        Assert.assertTrue(newUserH2.contains("New User Signup!"), "The h2 message is not as expected!");
         SignupLoginPage signupLoginPage = new SignupLoginPage(driver);
-        signupLoginPage.enterNameEmailAndClickSignup(firstName+" "+lastName,email);
-        Assert.assertTrue(driver.findElement(By.xpath("//b[contains(text(),'Enter Account Information')]")).isDisplayed(),"Enter Account Info text is not present!");
+        signupLoginPage.enterNameEmailAndClickSignup(firstName + " " + lastName, email);
+        Assert.assertTrue(driver.findElement(By.xpath("//b[contains(text(),'Enter Account Information')]")).isDisplayed(), "Enter Account Info text is not present!");
         SignupPage signupPage = new SignupPage(driver);
-        signupPage.enterSignupInfo(gender,password,dobDay,dobMonth,dobYear,firstName,lastName,companyName,companyAddress,countryName,state,city,zipCode,mobileNumber);
+        signupPage.enterSignupInfo(gender, password, dobDay, dobMonth, dobYear, firstName, lastName, companyName, companyAddress, countryName, state, city, zipCode, mobileNumber);
         String accountCreatedText = driver.findElement(By.xpath("//b[contains(text(),'Account Created!')]")).getText();
-        Assert.assertTrue(accountCreatedText.contains("ACCOUNT CREATED!"),"The h2 is not as expected!");
+        Assert.assertTrue(accountCreatedText.contains("ACCOUNT CREATED!"), "The h2 is not as expected!");
         AccountCreatedPage accountCreatedPage = new AccountCreatedPage(driver);
         accountCreatedPage.clickContinueButton();
         abstractReusable.clickDeleteAccount();
         String accountDeletedText = driver.findElement(By.xpath("//b[contains(text(),'Account Deleted!')]")).getText();
-        Assert.assertTrue(accountDeletedText.contains("ACCOUNT DELETED!"),"The h2 message is not as expected!");
+        Assert.assertTrue(accountDeletedText.contains("ACCOUNT DELETED!"), "The h2 message is not as expected!");
         AccountDeletedPage accountDeletedPage = new AccountDeletedPage(driver);
         accountDeletedPage.clickOnContinueButton();
 
     }
 
-    @Test
-    public void loginUserWithCorrectCredentials(){
+
+    //test below requires input of valid emails with passwords to work
+    @Test(dataProvider = "DataForTestCase2")
+    public void loginUserWithCorrectCredentials(String signInEmail, String signInPassword) {
         LandingPage landingPage = new LandingPage(driver);
         landingPage.openURL("https://automationexercise.com/");
-        Assert.assertEquals(driver.getTitle(),"Automation Exercise");
+        Assert.assertEquals(driver.getTitle(), "Automation Exercise");
         AbstractReusable abstractReusable = new AbstractReusable(driver);
         abstractReusable.clickOnSignupLoginButton();
         String loginText = driver.findElement(By.xpath("//h2[normalize-space()='Login to your account']")).getText();
-        Assert.assertEquals(loginText,"Login to your account");
+        Assert.assertEquals(loginText, "Login to your account");
         SignupLoginPage signupLoginPage = new SignupLoginPage(driver);
-        signupLoginPage.enterEmailPasswordClickSignIn("var@var.com","ramu");
+        signupLoginPage.enterEmailPasswordClickSignIn(signInEmail, signInPassword);
         String userNameOnWebsite = driver.findElement(By.tagName("b")).getText();
-        Assert.assertEquals(userNameOnWebsite,"var");
+        Assert.assertEquals(userNameOnWebsite, "var");
         abstractReusable.clickDeleteAccount();
         String accountDeletedText = driver.findElement(By.xpath("//b[contains(text(),'Account Deleted!')]")).getText();
-        Assert.assertTrue(accountDeletedText.contains("ACCOUNT DELETED!"),"The h2 message is not as expected!");
+        Assert.assertTrue(accountDeletedText.contains("ACCOUNT DELETED!"), "The h2 message is not as expected!");
         AccountDeletedPage accountDeletedPage = new AccountDeletedPage(driver);
         accountDeletedPage.clickOnContinueButton();
 
 
+    }
+
+
+    @Test(dataProvider = "DataForTestCase3")
+    public void loginUserWithWrongCredentials(String signInEmail, String signInPassword) {
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.openURL("https://automationexercise.com/");
+        Assert.assertEquals(driver.getTitle(), "Automation Exercise");
+        AbstractReusable abstractReusable = new AbstractReusable(driver);
+        abstractReusable.clickOnSignupLoginButton();
+        String loginText = driver.findElement(By.xpath("//h2[normalize-space()='Login to your account']")).getText();
+        Assert.assertEquals(loginText, "Login to your account");
+        SignupLoginPage signupLoginPage = new SignupLoginPage(driver);
+        signupLoginPage.enterEmailPasswordClickSignIn(signInEmail, signInPassword);
+
+        String errorMessage = driver.findElement(By.xpath("//p[@style='color: red;']")).getText();
+        Assert.assertEquals(errorMessage, "Your email or password is incorrect!");
 
 
     }
-
 
 
     @DataProvider(name = "DataForTestCase1")
     public Object[][] accountCreationData() {
-        return new Object[][] {
+        return new Object[][]{
                 {"male", "password123", "8", "April", "2000", "Rama", "Krishna",
                         "Sri Rama Krishna", "opposite TCS", "United States", "Andhra Pradesh",
-                        "Bhimavaram", "534201", "6696969696","ramaKrishna5@gmail.com"},
+                        "Bhimavaram", "534201", "6696969696", "ramaKrishna5@gmail.com"},
 
                 {"female", "password456", "15", "May", "1995", "Sita", "Devi",
                         "Sita Corporation", "near Infosys", "India", "Karnataka",
-                        "Bangalore", "560001", "9876543210","sitadevi@yahoo.com"},
+                        "Bangalore", "560001", "9876543210", "sitadevi@yahoo.com"},
 
                 {"male", "password789", "12", "June", "1988", "Lakshman", "Prasad",
                         "Lakshman Solutions", "beside Wipro", "India", "Tamil Nadu",
-                        "Chennai", "600001", "8765432109","lakshmiprasad@working.com"},
+                        "Chennai", "600001", "8765432109", "lakshmiprasad@working.com"},
 
                 {"female", "password321", "22", "July", "1993", "Radha", "Krishna",
                         "Radha Industries", "opposite Cognizant", "United States", "California",
-                        "San Francisco", "94105", "7654321098","radhakrishna@hotmail.com"}
+                        "San Francisco", "94105", "7654321098", "radhakrishna@hotmail.com"}
         };
     }
 
+    @DataProvider(name = "DataForTestCase2")
+    public Object[][] accountLoginDataCorrectCredentials() {
+        return new Object[][]{
+                {"var@var.com", "ramu"},
+                {"var@var.com", "ramu"}
+
+        };
 
 
+    }
 
+
+    @DataProvider(name = "DataForTestCase3")
+    public Object[][] accountLoginDataWrongCredentials() {
+
+        return new Object[][]{
+                {"sad@var.com", "????"},
+                {"rast@ymail.com", "#@!!"},
+                {"tara@hotmail.com", "456874"},
+                {"ramakirshna@yaol.com", "#@!!"},
+                {"tra@mail.com", "sa"}
+
+
+        };
+    }
 
 
 }
